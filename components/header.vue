c82b399f346ffd6193bbb4e6470212e7086bd279
MakotoPL <48807218+MakotoPD@users.noreply.github.com>
2023-08-21 13:08:08 +0200

    Update header.vue

--- components/header.vue
+++ components/header.vue
@@ -2,7 +2,9 @@
     <div>
         <div class="h-16 md:h-24 border-b px-14 lg:px-24 relative">
             <div class="md:w-fit h-full flex justify-center items-center">
-                <nuxt-img src="/img/logo.png" alt="RADEC24" class="h-5" />
+                <NuxtLink to="/">
+                    <nuxt-img src="/img/logo.png" alt="RADEC24" class="h-5" />
+                </NuxtLink>
             </div>
             <div class="hidden md:block desktopmenu absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
                 <div class="flex gap-5 justify-center items-center">
@@ -15,13 +17,13 @@
 
         <div class="md:hidden fixed bottom-8 w-full px-4 z-50">
             <div class="border w-full py-3 px-4 bg-neutral-200/60 backdrop-blur-sm rounded-3xl flex justify-center gap-12">
-                <NuxtLink to="/realizacje" class="px-1 py-1 bg-red-300/50 rounded-xl">
+                <NuxtLink to="/#kontakt" class="px-1 py-1 bg-red-300/50 rounded-xl focus:scale-125 duration-150">
                     <img src="/icons/phone.svg"/>
                 </NuxtLink>
-                <NuxtLink to="/" class="px-1 py-1 bg-red-300/50 rounded-xl">
+                <NuxtLink to="/" class="px-1 py-1 bg-red-300/50 rounded-xl focus:scale-125 duration-150">
                     <img src="/icons/house.svg"/>
                 </NuxtLink>
-                <a href="/#kontakt" class="px-1 py-1 bg-red-300/50 rounded-xl">
+                <a href="/realizacje" class="px-1 py-1 bg-red-300/50 rounded-xl focus:scale-125 duration-150">
                     <img src="/icons/images.svg"/>
                 </a>
             </div>
